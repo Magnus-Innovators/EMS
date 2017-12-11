eccf824e15d6ce1cd20e1993d5fa1e8e7eae8046
Shagul Hameed M <shagulhmd@Shaguls-MacBook-Pro.local>
2017-12-11 21:44:30 +0530

    Baseline layout of main application implemented

--- EMSApp/src/app/app.module.ts
+++ EMSApp/src/app/app.module.ts
@@ -7,18 +7,21 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { HomeModule } from './home/home.module';
 import { LinkService } from './shared-services/link.service';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: 'home', component: HomeComponent, loadChildren: './home/home.module#HomeModule'},
       {path: 'login', component: LoginComponent},
+      {path: 'main', component: MainComponent},
       {path: '', redirectTo: 'home', pathMatch: 'full'}
     ]),
     HomeModule
